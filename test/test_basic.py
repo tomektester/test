2ed9421531f7d148e6dd129b6c169bfab2d11cd4
Tomek Rej <tomek.rej@roamz.com>
2013-02-20 14:56:00 +1100

    First commit

---
+++ test/test_basic.py
@@ -0,0 +1,14 @@
+import unittest
+
+class BasicTest(unittest.TestCase):
+    def __init__(self):
+        pass
+
+    def setup(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_1(self):
+        self.assertEqual(1,1)
